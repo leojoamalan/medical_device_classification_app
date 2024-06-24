9050be9821d00b7b432cb5edca1104d822c0cde4
Leo S <123216029+leojoamalan@users.noreply.github.com>
2024-06-24 12:50:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ from inference_sdk import InferenceHTTPClient
 # initialize the client
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
-    api_key="kAbNHf7BK7n4dF8MzsMh"
+    api_key="EJSq61e3dlQXnJ0sOCAA"
 )
 
 # infer on a local image
